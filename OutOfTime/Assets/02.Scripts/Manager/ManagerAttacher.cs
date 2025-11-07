7ba255cf08b0c8e99062bb8f1429f86060040b99
Tejava01 <hewk0528@naver.com>
2025-11-08 04:27:33 +0900

    folder structure and manager attacher create

---
+++ OutOfTime/Assets/02.Scripts/Manager/ManagerAttacher.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ManagerAttacher : MonoBehaviour
+{
+    void Awake()
+    {
+        BootingUp();
+    }
+
+    //----------------------------------------------
+
+    private void BootingUp()
+    {
+        if (SceneManager.GetSceneByName("SceneAttacher").isLoaded == false)
+        {
+            Debug.Log("Managers Boot Complete");
+            SceneManager.LoadScene("SceneAttacher", LoadSceneMode.Additive);
+        }
+    }
+}
