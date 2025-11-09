aeecab84dc46c450317be68bd538e1b8ee13860d
monastary-master <michaelxie011@gmail.com>
2025-11-08 19:16:05 -0500

    Wave class and suggested usage

---
+++ OutOfTime/Assets/02.Scripts/Data Structures/Wave.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+
+// [Serializable] flag means it can be assigned in the inspector.
+// in BattleManager, add a field: [SerializeField] List<Wave> waves;
+[Serializable]
+public class Wave
+{
+    public GameObject enemy_prefab;
+    public int count;
+}
